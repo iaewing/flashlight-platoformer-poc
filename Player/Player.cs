3483b1ad22f5d2a4eb3876e837130241259bc4d3
Tim <nigh.timothy@gmail.com>
2023-04-19 16:12:37 -0400

    adjusted to 4 spaces for indentation

--- Player/Player.cs
+++ Player/Player.cs
@@ -2,71 +2,71 @@ using Godot;
 
 public partial class Player : CharacterBody2D
 {
-  [Signal]
-	public delegate void HitEventHandler();
+    [Signal]
+    public delegate void HitEventHandler();
 
-	[Export]
-	public int Speed = 400;
+    [Export]
+    public int Speed = 400;
 
-	public Vector2 ScreenSize;
+    public Vector2 ScreenSize;
 
-	private void OnPlayerBodyEntered(PhysicsBody2D body)
-	{
-		// Hide();
-		EmitSignal(SignalName.Hit);
-		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
-	}
+    private void OnPlayerBodyEntered(PhysicsBody2D body)
+    {
+        // Hide();
+        EmitSignal(SignalName.Hit);
+        GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+    }
 
-	public void Start(Vector2 position)
-	{
-		Position = position;
-		Show();
-		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
-	}
+    public void Start(Vector2 position)
+    {
+        Position = position;
+        Show();
+        GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+    }
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		ScreenSize = GetViewportRect().Size;
-		// Hide();
-	}
-	
-	public void GetInput()
-	{
-		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-		Velocity = inputDir * Speed;
-	}
-	
-	public override void _PhysicsProcess(double delta)
-	{
-		GetInput();
-		MoveAndCollide(Velocity * (float)delta);
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        ScreenSize = GetViewportRect().Size;
+        // Hide();
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    public void GetInput()
+    {
+        Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+        Velocity = inputDir * Speed;
+    }
 
-		if (Velocity.Length() > 0)
-		{
-			animatedSprite2D.Play();
-		}
-		else
-		{
-			animatedSprite2D.Stop();
-		}
+    public override void _PhysicsProcess(double delta)
+    {
+        GetInput();
+        MoveAndCollide(Velocity * (float)delta);
+    }
 
-		if (Velocity.X != 0)
-		{
-			animatedSprite2D.Animation = "walk";
-			animatedSprite2D.FlipV = false;
-			animatedSprite2D.FlipH = Velocity.X < 0;
-		}
-		else if (Velocity.Y != 0)
-		{
-			animatedSprite2D.Animation = "up";
-			animatedSprite2D.FlipV = Velocity.Y > 0;
-		}
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+        if (Velocity.Length() > 0)
+        {
+            animatedSprite2D.Play();
+        }
+        else
+        {
+            animatedSprite2D.Stop();
+        }
+
+        if (Velocity.X != 0)
+        {
+            animatedSprite2D.Animation = "walk";
+            animatedSprite2D.FlipV = false;
+            animatedSprite2D.FlipH = Velocity.X < 0;
+        }
+        else if (Velocity.Y != 0)
+        {
+            animatedSprite2D.Animation = "up";
+            animatedSprite2D.FlipV = Velocity.Y > 0;
+        }
+    }
 }
