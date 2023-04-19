1af9d30d1296725a9192714e680af52f13d6159b
Ian <iewing085@gmail.com>
2023-04-19 14:57:44 -0400

    WIP for Tim

--- Terrain/Lighting/Flashlight/Flashlight.cs
+++ Terrain/Lighting/Flashlight/Flashlight.cs
@@ -5,13 +5,11 @@ public partial class Flashlight : Sprite2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        //Grabs the parent aimatedsprite2d which SHOULD be the player
-        var player = GetParent() as CharacterBody2D;
-        //Sets the flashlight to the player's position
+        //TODO: This is a magic number of the offset between player and flashlight
         Position = new Vector2(67, 0);
     }
 
-        // Called every frame. 'delta' is the elapsed time since the previous frame.
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
         //Grabs the parent aimatedsprite2d which SHOULD be the player
@@ -19,18 +17,17 @@ public partial class Flashlight : Sprite2D
         //Sets the flashlight to the player's position
         // Position = player.GlobalPosition;
 
-        GD.Print("player velocity: " + player.Velocity);
-        GD.Print("player position: " + player.GlobalPosition);
-        GD.Print("flashlight position: " + this.Position);
+        GD.Print("player: " + player);
+        GD.Print("player velocity: " + player.Velocity.X + ',' + player.Velocity.Y);
 
         //Sets the flashlight's rotation to the player's rotation
         if (player.Velocity.X > 0)
         {
-            this.FlipV = false;
+            RotationDegrees = 90;
         }
         else if (player.Velocity.X < 0)
         {
-            Rotation = 180;
+            RotationDegrees = 180;
         }
         else if (player.Velocity.Y > 0)
         {
@@ -40,5 +37,6 @@ public partial class Flashlight : Sprite2D
         {
             Rotation = 270;
         }
+        GD.Print("flashlight rotation: " + Rotation);
     }
 }
